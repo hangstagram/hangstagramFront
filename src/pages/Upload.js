165918635b71f9ced023445a9424bfac7c5bff80
mmms2000 <moseskmcmmms@gmail.com>
2023-07-15 10:43:30 +0900

    Styled: upload text box and button

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,52 +1,104 @@
 import React, { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { uploadDataList } from "../redux/modules/dataListSlice";
+import styled from "styled-components";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function Upload() {
   const [content, setContent] = useState("");
   const [selectedImage, setSelectedImage] = useState(null);
 
   const dispatch = useDispatch();
-  const fileInputRef = useRef(null)
-  
-  const onChangeContent = (e) => setContent(e.target.value);
-  const onChangeImage = (e) => { setSelectedImage(e.target.files[0])
+  const fileInputRef = useRef(null);
+
+  const onChangeContent = (value) => setContent(value);
+  const onChangeImage = (e) => {
+    setSelectedImage(e.target.files[0]);
   };
 
   const uploadButtonHandler = () => {
-    dispatch(uploadDataList({
-      content,
-      selectedImage
-    }));
-    setContent('')
+    dispatch(
+      uploadDataList({
+        content,
+        selectedImage,
+      })
+    );
+    setContent("");
 
     if (fileInputRef.current) {
       fileInputRef.current.value = "";
     }
-  };
-  
 
-  
+    window.location.href = "/";
+  };
+
   return (
-    <>
-      <div>
-        <input
-          name="content"
-          value={content}
-          placeholder="내용"
-          onChange={onChangeContent}
-        />
-        <input 
-        ref={fileInputRef}
-        type="file" 
-        onChange={onChangeImage}/>
-        <button onClick={uploadButtonHandler}> 업로드</button>
-      </div>
+    <UploadLayout>
+      <UploadContainer>
+        <ImageStyled ref={fileInputRef} type="file" onChange={onChangeImage} />
+        <div style={{ width: "100%" }}>
+          <ReactQuill
+            theme="snow"
+            name="content"
+            value={content}
+            placeholder="내용"
+            onChange={onChangeContent}
+            style={{
+              width:'100%',
+              height:'260px',
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              paddingRight: "0px",
+              marginTop:'50px',
+            }}
+          >
+            <UploadButton onClick={uploadButtonHandler}>업로드</UploadButton>
+          </div>
+        </div>
+      </UploadContainer>
       {content}
-    </>
-  )
+    </UploadLayout>
+  );
 }
 
 export default Upload;
 
+const UploadLayout = styled.div`
+  margin: 0 auto;
+  width: 800px;
+  padding: 12px;
+  background-color: blue;
+`;
 
+const UploadContainer = styled.div`
+  background-color: yellow;
+  width: 97%;
+  display: flex;
+  gap: 6px;
+  padding: 12px;
+`;
+
+// const InputStyled = styled.input`
+//   background-color: green;
+//   width: 90%;
+//   height: 295px;
+// `;
+
+const UploadButton = styled.button`
+  background-color: black;
+  width: 80px;
+  height: 40px;
+  border-radius: 12px;
+  color: white;
+`;
+
+const ImageStyled = styled.input`
+  background-color: pink;
+  width: 60%;
+  height: 300px;
+`;
--- src/redux/modules/dataListSlice.js
+++ src/redux/modules/dataListSlice.js
@@ -1,25 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-        id:1,
-        content: "어려워요",
-        image: null
-    }],
-}
+  list: [
+    {
+      id: 1,
+      content: "어려워요",
+      image: null,
+    },
+  ],
+};
 
 const dataListSlice = createSlice({
-    name:"dataList",
-    initialState,
-    reducers: {
-        uploadDataList: (state,action) => {
-            state.list.push(action.payload);
-        },
-        deleteDataList : (state, action) =>{
-            state.list = state.list.filter((item)=> item.id !== action.payload)
-        },
-    }
-})
+  name: "dataList",
+  initialState,
+  reducers: {
+    uploadDataList: (state, action) => {
+      state.list = [...state.list, action.payload];
+    },
+    deleteDataList: (state, action) => {
+      state.list = state.list.filter((item) => item.id !== action.payload);
+    },
+  },
+});
 
-
-export const {uploadDataList} = dataListSlice.actions;
+export const { uploadDataList, deleteDataList } = dataListSlice.actions;
 export default dataListSlice.reducer;
