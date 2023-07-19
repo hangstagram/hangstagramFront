6a18e3ecd1852f4b45c6219ada9a2c409ee02009
mmms2000 <moseskmcmmms@gmail.com>
2023-07-20 00:22:58 +0900

    Folder: move Home componet to component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePen} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import { Penstyle } from "./Style";
-import Header from "../../Layout/Header"
-import Mapdata from "./Mapdata";
+import { Penstyle } from "../components/Home/Style";
+import Header from "../Layout/Header";
+import Mapdata from "../components/Home/Mapdata";
 
 function Homemain() {
 
--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -2,8 +2,7 @@ import { BrowserRouter,Route, Routes } from "react-router-dom";
 import Upload from "../pages/Upload/Uploadmain";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Homemain from "../pages/Home/Homemain";
-
+import Home from "../pages/Home"
 
 
 
@@ -13,7 +12,7 @@ const Router =()=>{
     return(
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Homemain/>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/upload" element={<Upload/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
